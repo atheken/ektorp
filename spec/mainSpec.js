03a9c2356e85a24819df3392d3f595ecad39c638
Andrew Theken <theken.1@osu.edu>
2014-02-02 19:58:38 -0500

    Added some testing, re-order one of the functions to be more modular.

--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -3,7 +3,7 @@ var nano = require('nano');
 //load the module under test.
 var ektorp = require('../lib/index.js');
 
-//set async timeout to a half-second.
+//async timeout will be a half-second.
 var HALF_SECOND = 500;
 
 describe("Migration function", function(){
@@ -38,29 +38,91 @@ describe("Migration function", function(){
 	}, HALF_SECOND);
 
 
-	xit("applies object-based migration.", function(done) {
-		done();
+	it("applies object-based migration.", function(done) {
+		//generate a "random" id.
+		var id = new Date().toString();
+
+		var migrator = ektorp(_conn, {label : 1, docs : { _id : id}});
+		migrator.on('done', function(){
+			_conn.get(id, function(err, result){
+				expect(result._rev).not.toBeNull();
+				expect(result._id).toEqual(id);
+				done();
+			});
+		});
+		migrator.start();
 	}, HALF_SECOND);
 	
-	xit("applies function-based migration.", function(done) {
-		done();
-	}, HALF_SECOND);
-	
-	xit("does not crash when no result is returned from a function.", function(done) {
-		done();
-	}, HALF_SECOND);
-	
-	xit("halts migrations when an exception is thrown from a migration.", function(done){
-		done();
+	it("applies array-based migration", function(done){
+		var testid1 = "testid1";
+		var testid2 = "testid2";
+		
+		var migrator = ektorp(_conn, {label: 1, docs : [ 
+			{ _id : testid2 },{_id : testid1 }
+		]});
+
+		migrator.on('done', function(){
+			_conn.list({keys : [testid1, testid2]}, function(err, result){
+				expect(result.rows.length).toEqual(2);
+				done();
+			});
+		});
+		migrator.start();
+
 	}, HALF_SECOND);
 
-	xit("applies 'up()' function when present on return object.", function(done){
-		done();
+	it("fails when no document is provided.", function(done) {
+		var migrator = ektorp(_conn, {label: 1, docs : null});
+
+		migrator.on('error', function(err){
+			expect(err.message).toEqual('Migrations must provide an array of docs, an object, '+
+										'or function that produces an array or object.');
+			done();
+		});
+
+		migrator.start();
 	}, HALF_SECOND);
 	
-	xit("applies 'down()' function when requested on return object.", function(done){
-		done();
+	it("applies function-based migration.", function(done) {
+		var id = 'text1234';
+
+		var appliedSpy = jasmine.createSpy();
+		var migrator = ektorp(_conn, [{label: 1, docs : function(){
+			appliedSpy();
+			return { _id : id};
+		}}]);
+	
+		migrator.on('done', function(){
+			expect(appliedSpy.callCount).toEqual(1);
+			_conn.get(id, function(err, result){
+				expect(result._id).toEqual(id);
+				done();
+			});
+		});
+
+		migrator.start();
 	}, HALF_SECOND);
+
+	it("halts migrations when an exception is thrown from a migration.", function(done){
+		var errorMessage = 'Function failed to get docs.';
+		var migrator = ektorp(_conn, [{
+								label: 1,
+								docs : []
+							}, 
+							{
+								label : 2,
+								docs : function(){ throw errorMessage; }
+							}] );
+		
+		migrator.on('error', function(err){
+			expect(err).not.toBeNull();
+			expect(err).toEqual("Function failed to get docs.");
+			done();
+		});
+
+		migrator.start();
+	}, HALF_SECOND);
+
 
 	it("creates a '_design/migrations' document to store information on a database.", function(done){
 
@@ -73,13 +135,12 @@ describe("Migration function", function(){
 				done();
 			});
 		});
-		
+
 		done();
 	}, HALF_SECOND);
 
 
 	it("parses version from file name.", function(done){
-
 		var versions = [201401261512,201401261513];
 		var migrator = ektorp(_conn, __dirname + '/testMigrations');
 		
@@ -127,4 +188,23 @@ describe("Migration function", function(){
 		expect(migrator instanceof EventEmitter).toBe(true);
 	});
 
+	xit("function-based migrations are provided a reference to the database.", function(done){
+
+	}, HALF_SECOND);
+
+	xit("function-based migrations are provided a callback when their arity is two.", function(done){
+
+	}, HALF_SECOND);
+
+	xit("skips migrations that have already been applied.", function(done){
+
+	}, HALF_SECOND);
+
+	xit("updates existing documents", function(done){
+
+	}, HALF_SECOND);
+
+	xit("skips migration when it would not modify target document", function(done){
+
+	}, HALF_SECOND);
 });
